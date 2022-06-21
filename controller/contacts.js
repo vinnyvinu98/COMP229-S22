530ef456874d4fd3d59952aca6d20015fbaad827
Vinny Mariam Vinu <vinnyvinu98@gmail.com>
2022-06-21 01:43:07 -0400

    Assignment 2 Final

--- controller/contacts.js
+++ controller/contacts.js
@@ -2,85 +2,77 @@
 const { default: mongoose } = require('mongoose');
 let ContactModel = require('../models/contacts');
 
-module.exports.contactsList = function(req, res, next){
+module.exports.contactsList = function (req, res, next) {
 
-    ContactModel.find( (err, contactsList)=>{
+    ContactModel.find((err, contactsList) => {
 
-        if(err){
+        if (err) {
             return console.error(err);
         }
-        
-        else{
+
+        else {
             res.render(
-                'contacts/list', 
-                { 
-                  title: 'Contact List',
-                  ContactsList:contactsList
+                'contacts/list',
+                {
+                    title: 'Contact List',
+                    ContactsList: contactsList
                 }
-              );
+            );
 
         }
-    }).sort('name') 
+    }).sort('name')
 
 }
 
 module.exports.displayEditPage = (req, res, next) => {
     let id = req.params.id;
+    
+        ContactModel.findById(id, (err, ContactToEdit) => {
+            if(err)
+            {
+                console.log(err);
+                res.end(err);
+            }
+            else
+            {
+                //show the edit view
+                res.render('contacts/editcontacts', {
+                    title: 'Edit Contact', 
+                    contact: ContactToEdit
+                })
+            }
+        });
 
-    ContactModel.findById(id, (err, ContactToEdit) => {
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
-        }
-        else
-        {
-            //show the edit view
-            res.render('contacts/editcontacts', {
-                title: 'Edit Contact', 
-                contact: ContactToEdit
-            })
-        }
-    });
 }
 
 
 
 module.exports.processEditPage = (req, res, next) => {
     let id = req.params.id
-
-    let updatedContact = ContactModel({
-        _id: body.id,
+    let updatedContact = {
         name: req.body.name,
         address: req.body.address,
         email: req.body.email
-    });
-    console.log(id)
-    ContactModel.updateOne({_id: id}, updatedContact, (err) => {
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
+    };
+    ContactModel.updateOne({_id:mongoose.Types.ObjectId(id)},updatedContact,(err,result)=>{
+        if(err){
+            console.log(err)
+        }else if(result){
+            console.log(result)
+            res.redirect('/contacts/list')
         }
-        else
-        {
-            console.log( id);
-            res.redirect('/contacts/list');
-        }
-    });
+    })
 }
 
 module.exports.performDelete = (req, res, next) => {
     let id = req.params.id;
 
-    ContactModel.remove({_id: id}, (err) => {
-        if(err)
-        {
+    ContactModel.remove({ _id: id }, (err) => {
+        if (err) {
             console.log(err);
             res.end(err);
         }
-        else
-        {
+        else {
             res.redirect('/contacts/list');
         }
     });
